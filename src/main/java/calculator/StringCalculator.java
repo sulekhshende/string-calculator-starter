daeb71c06b9b393209ac43c2a39020b368dc8ebd
Sulekh Shende <shendesulekh@gmail.com>
2021-10-17 10:56:34 +0530

    testcase1_complete

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,35 @@
 package calculator;
 
+import java.util.Iterator;
+
 class StringCalculator {
-
+	
     public int add(String input) {
-        return 0;
+        if(input == ""){
+        	return 0;
+        }else{
+        	String delimiter = ",";
+        	String []inputNumbers = splitNumbers(input,delimiter);
+        	return sumofinputs(inputNumbers);
+        } 		
     }
+    
+	private int sumofinputs(String[] inputNumbers) {
+		int sum = 0;
+		for(String number : inputNumbers) {
+			sum += parsingint(number);
+		}
+		return sum;
+	}
 
+	private String[] splitNumbers(String input, String delimiter) {
+			return input.split(delimiter);
+	}
+
+	private int parsingint(String input) {
+		return Integer.parseInt(input);
+	}
+	
+    
+    
 }
